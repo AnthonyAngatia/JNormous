222ebf7d461264e4e6b16ed103bb9a34dfb0a0e7
Anthony Angatia <anthony.angatia@cellulant.io>
2024-12-14 10:58:56 +0300

    Initial project configuration

---
+++ AboutMonoFlux/src/main/java/com/example/aboutmonoflux/AboutMonoFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.aboutmonoflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutMonoFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AboutMonoFluxApplication.class, args);
+    }
+
+}
---
+++ AboutMonoFlux/src/test/java/com/example/aboutmonoflux/AboutMonoFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aboutmonoflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutMonoFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
