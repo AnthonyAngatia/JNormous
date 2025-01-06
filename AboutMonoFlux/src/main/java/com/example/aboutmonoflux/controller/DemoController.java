171c8ecbdb6c9bfcec9317a5e6ff03d71973e860
Anthony Angatia <anthony.angatia@cellulant.io>
2025-01-06 17:02:04 +0300

    Reactive Java experiments

--- AboutMonoFlux/src/main/java/com/example/aboutmonoflux/controller/DemoController.java
+++ AboutMonoFlux/src/main/java/com/example/aboutmonoflux/controller/DemoController.java
@@ -1,7 +1,27 @@
 package com.example.aboutmonoflux.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.stream.Stream;
 
 @RestController
 public class DemoController {
+
+    @GetMapping("/demo")
+    public void demo(){
+        // Examples of how a publisher can be created
+        Flux<Integer> f1 = Flux.just(1, 2, 3);
+        var f2 = Flux.fromStream(Stream.of(1,2,3));
+
+
+        f1.doOnNext(System.out::println).subscribe();
+        f1.subscribe(x->{
+            System.out.println("Second one");
+            System.out.println(x);
+        });
+
+
+    }
 }
